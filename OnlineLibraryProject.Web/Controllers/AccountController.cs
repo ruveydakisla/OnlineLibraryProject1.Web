53076184c151d7d52066d0a58cc30aee13a70b86
Ruveyda <ruveydakisla34@gmail.com>
2022-12-06 11:21:00 +0300

    Add project files.

---
+++ OnlineLibraryProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibraryProject.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+
+}
+
