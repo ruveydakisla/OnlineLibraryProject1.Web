34900c83ded29fad47445b1679e7977545835e85
Omer Faruk KUMRAL <the.kumral44@gmail.com>
2022-12-07 15:41:01 +0300

    entity structure fixed

---
+++ OnlineLibraryProject.Web/Migrations/20221207090356_PageNumberAddForBook.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineLibraryProject.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class PageNumberAddForBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PageNumber",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageNumber",
+                table: "Books");
+        }
+    }
+}
